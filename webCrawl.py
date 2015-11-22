8c7546bea1dec94e6bbcdc0b9806b8a67fb3c8a9
nakte <ankita.joshi1591@gmail.com>
2015-11-21 19:57:54 -0500

    third commit

--- webCrawl.py
+++ webCrawl.py
@@ -3,7 +3,18 @@ import codecs
 import nltk
 import urllib2
 
+from bs4 import BeautifulSoup
+
+#soup = BeautifulSoup("https://mr.wikipedia.org/s/2cv")
+#text = soup.getText()
+#print(text)
+
 f = urllib2.urlopen("https://mr.wikipedia.org/s/2cv")
-print nltk.clean_html(f.read())
+
+soup = BeautifulSoup(f)
+text = soup.getText()
+print(text)
+
+#print nltk.clean_html(f.read())
 
 
